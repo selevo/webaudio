28fba0d67e7005459ebf2e850e095848b8893004
Serega <selevo@mail.ru>
2025-08-12 16:40:15 +0300

    Update serial_to_audio.js
    Поправил   скобки чтобы было   понятнее где  что начинется.

--- serial_to_audio.js
+++ serial_to_audio.js
@@ -40,47 +40,51 @@ Send the given string of data out over audio.
 функция ниже передает данные  и  что-то возвращает в переменной data, callback - непонятно пока что
 
  */
-function audio_serial_write(data, callback) {
-  var sampleRate = 44100;
-  var header = sampleRate; // 1 sec to charge/discharge the cap
-  var baud = 9600;
-  var samplesPerByte = parseInt(sampleRate*11/baud);
-  var bufferSize = samplesPerByte*data.length/*samples*/ + header*2;
-  var buffer = context.createBuffer(1, bufferSize, sampleRate);
-  var b = buffer.getChannelData(0);
+function audio_serial_write(data, callback)
+    {
+      var sampleRate = 44100;
+      var header = sampleRate; // 1 sec to charge/discharge the cap
+      var baud = 9600;
+      var samplesPerByte = parseInt(sampleRate*11/baud);
+      var bufferSize = samplesPerByte*data.length/*samples*/ + header*2;
+      var buffer = context.createBuffer(1, bufferSize, sampleRate);
+      var b = buffer.getChannelData(0);
 
-  for (var i=0;i<header;i++) b[i]=i / header;
+      for (var i=0;i<header;i++) b[i]=i / header;
 
-  var offset = header;
+      var offset = header;
 
-  data.split("").forEach(function(c) {
-    var byte = c.charCodeAt(0);
-    if (byte>=0 && byte<=255) {    
-      for (var i=0;i<samplesPerByte;i++) {
-        var bit = Math.round(i*baud/sampleRate);
-        var value = 1;
-        if (bit==0) value=0; // start bit
-        else if (bit==9 || bit==10) value=1; // stop bits
-        else value = (byte&(1<<(bit-1))) ? 1 : 0; // data
-        b[offset++] = value*2-1; 
-      }
-    } else {
-      // just insert a pause
-      for (var i=0;i<samplesPerByte;i++) 
-        b[offset++] = 1; 
-    }
-  });
+      data.split("").forEach(function(c)
+       {
+            var byte = c.charCodeAt(0);
+            if (byte>=0 && byte<=255)
+            {    
+              for (var i=0;i<samplesPerByte;i++)
+                  {
+                    var bit = Math.round(i*baud/sampleRate);
+                    var value = 1;
+                    if (bit==0) value=0; // start bit
+                    else if (bit==9 || bit==10) value=1; // stop bits
+                    else value = (byte&(1<<(bit-1))) ? 1 : 0; // data
+                     b[offset++] = value*2-1; 
+                  }
+            }     else {
+                      // just insert a pause
+                      for (var i=0;i<samplesPerByte;i++) 
+                    b[offset++] = 1; 
+                   }    
+      });
 
-  for (var i=0;i<header;i++) b[offset+i]=1-(i / header);
+      for (var i=0;i<header;i++) b[offset+i]=1-(i / header);
 
-  if (audio_serial_invert)
-    for (var i=0;i<bufferSize;i++) b[i] = 1-b[i];
+      if (audio_serial_invert)
+        for (var i=0;i<bufferSize;i++) b[i] = 1-b[i];
 
-  var source = context.createBufferSource();
-  source.buffer = buffer;
-  source.connect(context.destination);
-  source.start();
+      var source = context.createBufferSource();
+      source.buffer = buffer;
+      source.connect(context.destination);
+      source.start();
 
-  if (callback)
-    window.setTimeout(callback, 1000*bufferSize/sampleRate);
+      if (callback)
+        window.setTimeout(callback, 1000*bufferSize/sampleRate);
 }
